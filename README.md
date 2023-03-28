a418beb5ba6ba055f1dc5e1a00cb0225d5f7b9d6
Sasha Dranuta <dranuta97@gmail.com>
2023-03-28 16:47:08 +0300

    My work

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## [DEMO LINK](https://<replace_your_name>.github.io/react_live_coding_cars_table_task/)
+## [DEMO LINK](https://sasha-dranuta.github.io/react_live_coding_cars_table_task/)
 
 ## Instructions
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,50 @@
-import React from 'react';
-// import carsFromServer from './api/cars';
-// import colorsFromServer from './api/colors';
+import React, { useState } from 'react';
+import carsFromServer from './api/cars';
+import colorsFromServer from './api/colors';
 
 // 1. Render car with color
 // 2. Add ability to filter car by brand name
 // 3. Add ability to filter car by color
 
+interface Color {
+  id: number,
+  name: string,
+}
+
+interface Car {
+  id: number,
+  brand: string,
+  rentPrice: number,
+  colorId: number,
+}
+
+function getColorId(colorId: number): Color | undefined {
+  const foundColor = colorsFromServer.find(({ id }) => colorId === id);
+
+  return foundColor;
+}
+
+const cars: Car[] = carsFromServer.map(car => {
+  return {
+    ...car,
+    color: getColorId(car.colorId),
+  };
+});
+
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+
+  function getBrandQuery(event) {
+    setQuery(event.target.value);
+  }
+
   return (
     <div>
-      <input type="search" placeholder="Find by car brand" />
+      <input
+        type="search"
+        placeholder="Find by car brand"
+        onChange={getBrandQuery}
+      />
 
       <select>
         <option>Chose a color</option>
@@ -25,24 +60,22 @@ export const App: React.FC = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Ferarri</td>
-            <td style={{ color: 'red' }}>Red</td>
-            <td>500</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Opel</td>
-            <td style={{ color: 'white' }}>White</td>
-            <td>300</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>Audi</td>
-            <td style={{ color: 'black' }}>Black</td>
-            <td>300</td>
-          </tr>
+          {cars
+            .filter((car) => {
+              return car.brand.toLowerCase().includes(query.toLowerCase())
+                ? car
+                : '';
+            })
+            .map((car) => {
+              return (
+                <tr key={car.id}>
+                  <td>{car.id}</td>
+                  <td>{car.brand}</td>
+                  <td style={{ color: car.color.name }}>{car.color.name}</td>
+                  <td>{car.rentPrice}</td>
+                </tr>
+              );
+            })}
         </tbody>
       </table>
     </div>
